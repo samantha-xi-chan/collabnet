43cc971fba4351e3e0e7e8f2633d43234583119e
Your Name <you@example.com>
2023-10-18 22:49:26 +0800

    add log

--- cmd/time.go
+++ cmd/time.go
@@ -28,12 +28,17 @@ func init() {
 		"amqp://RABBITMQ_USER:RABBITMQ_PASS@rmq-cluster:5672/",
 		config_sched.AMQP_EXCH,
 		"root:password@tcp(mysql:3306)/biz?charset=utf8mb4&parseTime=True&loc=Local")
+
+	log.Println("service_time.Init ok ")
 }
 
 func main() {
+	log.Println("[main] start ... ")
 
 	go func() {
-		e := control_time.InitTimeHttpService(":8088")
+		addr := ":8088"
+		log.Printf("goting to InitTimeHttpService on  %s\n", addr)
+		e := control_time.InitTimeHttpService(addr)
 		if e != nil {
 			log.Fatal("control_time.InitTimeHttpService e: ", e)
 		}
