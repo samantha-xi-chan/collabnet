185fd6cebc2fa364a237e84029ac3b143a714dba
Your Name <you@example.com>
2023-12-10 18:28:04 +0800

    update test

--- test/workflow_case04/start.sh
+++ test/workflow_case04/start.sh
@@ -1,4 +1,12 @@
-./$1
+#./$1
+
+current_dir="$(cd "$(dirname "$0")" && pwd)"
+echo "\$current_dir：$current_dir"
+cd $current_dir
+pwd
+nohup sh -c "while true; do sh ./test_dag_workflow.sh; done" > ./$(date -u +'%Y-%m-%dT%H:%M:%SZ').log &
+
+DAG=$current_dir"/user_provide/dag.json"
 
 
 
